73e4b59489cf59155116333ae5723be92a375745
Tony Rivera <juarivera@tesla.com>
2019-06-05 11:37:55 -0700

    Initial Commit

---
+++ iOSegmentControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSegmentControl
+//
+//  Created by Tony Rivera on 6/5/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
